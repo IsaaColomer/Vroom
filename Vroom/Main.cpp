44c57088aa5e710d511d0f94a62dfde55a387c29
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-09-21 09:10:45 +0200

    Repeat

--- Vroom/Main.cpp
+++ Vroom/Main.cpp
@@ -6,17 +6,6 @@
 #pragma comment( lib, "SDL/libx86/SDL2.lib" )
 #pragma comment( lib, "SDL/libx86/SDL2main.lib" )
 
-
-#include "imgui/imgui.h"
-#include "imgui/backends/imgui_impl_sdl.h"
-#include "imgui/backends/imgui_impl_opengl3.h"
-
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <SDL_opengles2.h>
-#else
-//#include <SDL_opengl.h>
-#endif
-
 enum main_states
 {
 	MAIN_CREATION,
@@ -25,74 +14,75 @@ enum main_states
 	MAIN_FINISH,
 	MAIN_EXIT
 };
-int main(int argc, char** argv)
+
+int main(int argc, char ** argv)
 {
-    LOG("Starting game '%s'...", TITLE);
+	LOG("Starting game '%s'...", TITLE);
 
-    int main_return = EXIT_FAILURE;
-    main_states state = MAIN_CREATION;
-    Application* App = NULL;
+	int main_return = EXIT_FAILURE;
+	main_states state = MAIN_CREATION;
+	Application* App = NULL;
 
-    while (state != MAIN_EXIT)
-    {
-        switch (state)
-        {
-        case MAIN_CREATION:
+	while (state != MAIN_EXIT)
+	{
+		switch (state)
+		{
+		case MAIN_CREATION:
 
-            LOG("-------------- Application Creation --------------");
-            App = new Application();
-            state = MAIN_START;
-            break;
+			LOG("-------------- Application Creation --------------");
+			App = new Application();
+			state = MAIN_START;
+			break;
 
-        case MAIN_START:
+		case MAIN_START:
 
-            LOG("-------------- Application Init --------------");
-            if (App->Init() == false)
-            {
-                LOG("Application Init exits with ERROR");
-                state = MAIN_EXIT;
-            }
-            else
-            {
-                state = MAIN_UPDATE;
-                LOG("-------------- Application Update --------------");
-            }
+			LOG("-------------- Application Init --------------");
+			if (App->Init() == false)
+			{
+				LOG("Application Init exits with ERROR");
+				state = MAIN_EXIT;
+			}
+			else
+			{
+				state = MAIN_UPDATE;
+				LOG("-------------- Application Update --------------");
+			}
 
-            break;
+			break;
 
-        case MAIN_UPDATE:
-        {
-            int update_return = App->Update();
+		case MAIN_UPDATE:
+		{
+			int update_return = App->Update();
 
-            if (update_return == UPDATE_ERROR)
-            {
-                LOG("Application Update exits with ERROR");
-                state = MAIN_EXIT;
-            }
+			if (update_return == UPDATE_ERROR)
+			{
+				LOG("Application Update exits with ERROR");
+				state = MAIN_EXIT;
+			}
 
-            if (update_return == UPDATE_STOP)
-                state = MAIN_FINISH;
-        }
-        break;
+			if (update_return == UPDATE_STOP)
+				state = MAIN_FINISH;
+		}
+			break;
 
-        case MAIN_FINISH:
+		case MAIN_FINISH:
 
-            LOG("-------------- Application CleanUp --------------");
-            if (App->CleanUp() == false)
-            {
-                LOG("Application CleanUp exits with ERROR");
-            }
-            else
-                main_return = EXIT_SUCCESS;
+			LOG("-------------- Application CleanUp --------------");
+			if (App->CleanUp() == false)
+			{
+				LOG("Application CleanUp exits with ERROR");
+			}
+			else
+				main_return = EXIT_SUCCESS;
 
-            state = MAIN_EXIT;
+			state = MAIN_EXIT;
 
-            break;
+			break;
 
-        }
-    }
+		}
+	}
 
-    delete App;
-    LOG("Exiting game '%s'...\n", TITLE);
-    return main_return;
+	delete App;
+	LOG("Exiting game '%s'...\n", TITLE);
+	return main_return;
 }
